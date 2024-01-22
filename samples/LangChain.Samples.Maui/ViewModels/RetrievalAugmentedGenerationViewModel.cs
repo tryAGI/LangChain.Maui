c1f60da5282712caaceef10224d9dcf298bb0dc2
HavenDV <havendv@gmail.com>
2024-01-22 05:58:56 +0400

    feat: Added prompt view to RAG.

--- samples/LangChain.Samples.Maui/ViewModels/RetrievalAugmentedGenerationViewModel.cs
+++ samples/LangChain.Samples.Maui/ViewModels/RetrievalAugmentedGenerationViewModel.cs
@@ -28,6 +28,9 @@ public partial class RetrievalAugmentedGenerationViewModel(
 	private string _question = string.Empty;
 	
 	[ObservableProperty]
+	private string _prompt = string.Empty;
+	
+	[ObservableProperty]
 	private string _answer = string.Empty;
 
 	private bool CanSelectPdfFromComputer()
@@ -120,9 +123,7 @@ public partial class RetrievalAugmentedGenerationViewModel(
 			var similarDocuments = await _database.GetSimilarDocuments(
 				Question, amount: 5);
 
-			Status = "Generating answer...";
-			
-			var response = await gpt35.GenerateAsync(
+			Prompt =
 				$"""
 				 Use the following pieces of context to answer the question at the end.
 				 If the answer is not in context then just say that you don't know, don't try to make up an answer.
@@ -132,7 +133,11 @@ public partial class RetrievalAugmentedGenerationViewModel(
 
 				 Question: {Question}
 				 Helpful Answer:
-				 """, CancellationToken.None).ConfigureAwait(false);
+				 """;
+			Status = "Generating answer...";
+			
+			var response = await gpt35.GenerateAsync(
+				Prompt, CancellationToken.None).ConfigureAwait(false);
         
 			Answer = response.LastMessageContent;
 			Status = "Answer generated";
